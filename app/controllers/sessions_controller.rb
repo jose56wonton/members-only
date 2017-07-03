f264c5760e34f972943ecf36aab78df3994791ab
Joshua Wootonn <joshuawootonn@gmail.com>
2017-07-02 19:36:24 -0500

    Finish final login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] ? remember(user) : forget(user)
       flash[:success] = "Welcome back #{user.username}"
       redirect_to root_url
     else
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
